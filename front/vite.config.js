7788010498376727f2b3ae532335d38ad6dd6969
Ajala Emmanuel Olushola <ajalaemmanueloluwashola@gmail.com>
2025-04-08 05:47:14 +0000

    Fix: update vite.config.js and remove backend proxy config

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4040',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\api/,'')
-      }
-    }
+  build: {
+    outDir: 'dist',
+    minify: false,      // helps avoid memory issues on Render
+    target: 'esnext'    // use modern JS (faster build)
   }
 })
